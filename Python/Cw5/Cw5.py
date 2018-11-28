61ba5fac65fb5ca8f86739560345b698ff2b6378
Tomasz Karkocha <karkochat@gmail.com>
2018-11-28 03:09:42 +0100

    Added second boundary condition for third degree

--- Python/Cw5/Cw5.py
+++ Python/Cw5/Cw5.py
@@ -7,8 +7,14 @@ from SecondDegreeSpline import second_degree_spline
 
 def f(x):
     # return x**14 + x**3 + 12 * x - 1
-    return np.exp(4 * np.cos(5 * x))
-    # return np.sin(x * 2) * np.sin((2 * x ** 2) / np.pi)
+    # return np.exp(4 * np.cos(5 * x))
+    return np.sin(x * 2) * np.sin((2 * x ** 2) / np.pi)
+
+
+def df(x):
+    a = 4 * x * np.sin(2*x) * np.cos(2*x**2/np.pi) / np.pi
+    b = 2 * np.sin(2*x**2/np.pi) * np.cos(2*x)
+    return a + b
 
 
 # =========================================
@@ -23,21 +29,32 @@ def list_of_coords(a, b, n):
     return points
 
 
+def list_of_chebyshev_coords(a, b, k):
+    points = []
+    for j in range(1, k + 1):
+        x = np.cos(((2 * j - 1) * np.pi) / float(2 * k)) * (b-a)/2 + (a+b)/2
+        points.append((x, f(x)))
+    return points
+
+
 # =========================================
 def main():
     start, end = -np.pi, np.pi
-    n = 9
+    n = 2
     M = 1000
 
-    x = lambda n: np.linspace(start, end, n)
-    LX = x(M)
-    p = list_of_coords(start, end, n)
-    # LY = second_degree_spline(p, LX, 0)
-    LY = third_degree_spline(p, LX)
-    plot(LX, LY, M, n, p, x)
+    ns = [4, 5, 7, 10, 13, 15, 20, 30]
+    for n in ns:
+        x = lambda n: np.linspace(start, end, n)
+        LX = x(M)
+       # p = list_of_chebyshev_coords(start, end, n)
+        p = list_of_coords(start, end, n)
+        #LY = second_degree_spline(p, LX, 5)
+        LY = third_degree_spline(p, LX)
+        plot(LX, LY, M, n, p, x, 3, 'natural')
 
 
-def plot(LX, LY, M, n, points, x):
+def plot(LX, LY, M, n, points, x, degree, type):
     for i in range(n):
         plt.axvline(x=points[i][0], color='b')
 
@@ -46,7 +63,9 @@ def plot(LX, LY, M, n, points, x):
 
     plt.plot(LX, LY, 'b')
 
-    plt.show()
+    name = "plot" + str(n) + "_" + str(degree) + "_" + type
+    plt.savefig("./plots/" + name)
+    #plt.show()
 
 
 if __name__ == "__main__":
--- Python/Cw5/ThirdDegreeSpline.py
+++ Python/Cw5/ThirdDegreeSpline.py
@@ -1,13 +1,17 @@
 import numpy as np
 
-def third_degree_spline(points, LX):
+
+def third_degree_spline(points, LX, dx=None, dxn=None):
     n = len(points) - 1
     nx = len(LX)
 
     t = [d[0] for d in points]
     y = [d[1] for d in points]
     h = [(t[i+1] - t[i]) for i in range(n)]
-    z = tridiagonal_solver(h, y, n + 1)
+    if (dx is None and dxn is None):
+        z = tridiagonal_solver_natural(h, y, n)
+    else:
+        z = tridiagonal_solver_clamped(h, y, n, dx, dxn)
 
     def S(i, x):
         p1 = z[i+1] * (x - t[i])**3 / (6 * h[i]) + z[i] * (t[i+1] - x)**3 / (6 * h[i])
@@ -24,24 +28,53 @@ def third_degree_spline(points, LX):
                 break
     L[0] = y[0]
 
+    print(L)
     return L
 
 
-def tridiagonal_solver(h, y, n):
-    u = np.zeros(n)
-    v = np.zeros(n)
+def tridiagonal_solver_natural(h, y, n):
+    u = np.zeros(n - 1)
+    v = np.zeros(n - 1)
 
-    for i in range(1, n-1):
-        u[i] = 2 * (h[i-1] + h[i])
-        v[i] = 6 * ((y[i+1] - y[i]) / h[i]) - ((y[i] - y[i-1]) / h[i-1])
+    for i in range(0, n - 1):
+        u[i] = 2 * (h[i] + h[i + 1])
+        v[i] = 6 * ((y[i+2] - y[i+1]) / h[i+1]) - ((y[i+1] - y[i]) / h[i])
 
-    for i in range(2, n-1):
+    for i in range(1, n - 2):
         u[i] -= h[i-1]**2 / u[i-1]
         v[i] -= h[i-1] * v[i-1] / u[i-1]
 
-    z = np.zeros(n)
-    z[n-2] = v[n-2] / u[n-2]
+    z = np.zeros(n + 1)
+    z[n-1] = v[n-2] / u[n-2]
     for i in range(n-3, 0, -1):
-        z[i] = (v[i] - h[i] * z[i+1]) / u[i]
+        z[i + 1] = (v[i] - h[i] * z[i + 2]) / u[i]
 
     return z
+
+
+# d[i] = (y[i+1] - y[i]) / h[i]
+def tridiagonal_solver_clamped(h, y, n, dx1, dxn):
+    u = np.zeros(n - 1)
+    v = np.zeros(n - 1)
+
+    for i in range(0, n - 1):
+        u[i] = 2 * (h[i] + h[i + 1])
+        v[i] = 6 * ((y[i+2] - y[i+1]) / h[i+1]) - ((y[i+1] - y[i]) / h[i])
+
+    v[0] -= 3 * ((y[1] - y[0]) / h[0] - dx1)
+    v[n-2] -= v[n-2] - 3 * (dxn - (y[n] - y[n-1]) / h[n-1])
+
+    for i in range(1, n - 2):
+        u[i] -= h[i-1]**2 / u[i-1]
+        v[i] -= h[i-1] * v[i-1] / u[i-1]
+
+    z = np.zeros(n + 1)
+    z[n-1] = v[n-2] / u[n-2]
+    for i in range(n-3, 0, -1):
+        z[i+1] = (v[i] - h[i] * z[i+2]) / u[i]
+
+    z[0] = (3 * (y[1] - y[0]) / h[0] - dx1) / h[0] - z[1] / 2
+    z[n] = (3 * (dxn - (y[n] - y[n-1]) / h[n-1])) / h[n-1] - z[n-1] / 2
+
+    return z
+
